22efb11cb327c6f880787cf46881048a07b10df5
Rafael Dias <rafa197795@gmail.com>
2018-09-26 23:01:17 -0300

    Merge

--- lab1.asm
+++ lab1.asm
@@ -9,21 +9,30 @@
 		"img_out.bmp"
 	buffer:
 		.space SYS_BUFFER_SIZE			# Tamanho do buffer 
+	textMenu:
+		.asciiz "Escolha uma das opções abaixo:\n\t[0] Sair\t[1] Blur\t[2] Edge Extractor\t[3] Thresholding\n" # Texto que sera mostrado no menu
+	cls: 
+		.asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" # espaçammento para parecer novo menu apos escolha do usuario
+	textMenuError:
+		.asciiz "\nOpção escolhida inválida, aperta '1' para continuar\n"
+
+	textTest:
+		.asciiz "\nOpção escolhida foi:"
 
 .text
 	main:
 	# ---------------------------------------------
 	# TODO:
-	# 1. Menu de escolhas de operações (Blur, Edge Extractor e Thresholding (terminal)
-	# 2. Visualizar no bitmap display e salvar em um arquivo com o nome que o usuário definir (terminal)
-	# 3. Efeito de Borramento (Blur effect) com os parâmetros da máscara definido pelo usuário
-	# 4. Efeito de Extração de Bordas (Edge Extractor) com os parâmetros da máscara definido pelo usuário
-	# 5. Binarização por limiar (Thresholding) definido pelo usuário
+	# 1. Menu de escolhas de operaÃ§Ãµes (Blur, Edge Extractor e Thresholding (terminal)
+	# 2. Visualizar no bitmap display e salvar em um arquivo com o nome que o usuÃ¡rio definir (terminal)
+	# 3. Efeito de Borramento (Blur effect) com os parÃ¢metros da mÃ¡scara definido pelo usuÃ¡rio
+	# 4. Efeito de ExtraÃ§Ã£o de Bordas (Edge Extractor) com os parÃ¢metros da mÃ¡scara definido pelo usuÃ¡rio
+	# 5. BinarizaÃ§Ã£o por limiar (Thresholding) definido pelo usuÃ¡rio
 	# ---------------------------------------------
 	
 	# ---------------------------------------------
 	# $s0 = Arquivo de entrada
-	# $s1 = Arquivo de saída
+	# $s1 = Arquivo de saÃ­da
 	# ---------------------------------------------
 	la		$s0, filein			
 	la		$s1, fileout
@@ -45,7 +54,7 @@
 				
 	jal		closeFile		
 	
-	j		fim	
+	j		menu	
 #######################################################################	
 	openFile:
 	li		$v0, 13
@@ -78,6 +87,54 @@
 	syscall
 	
 	jr		$ra
+
+#######################################################################
+	menu:
+	la $a0, cls   # carregar o espaçamento
+	li $v0, 4     # print do espaçamento
+	syscall
+	
+	la $a0, textMenu
+	syscall
+	
+	li $v0, 5    # Esperando resposta do usuario
+	syscall
+	
+	beq $v0, 0, fim  # Sai do programa
+	
+	beq $v0, 1, teste  # faz o Blur
+	#jal blue
+	
+	beq $v0, 2, teste  # faz o Edge Extractor
+	#jal edge
+	
+	beq $v0, 3, teste  #faz o Thresholding
+	#jal thresholding
+	
+	bgt $v0, 3, notOption
+#######################################################################
+	teste:
+	move $t0, $v0
+	li $v0, 4
+	la $a0, textTest
+	syscall
+	
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	
+	b menu
+#######################################################################	
+	notOption:
+	li $v0, 4
+	la $a0, textMenuError
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	b menu
+	
 #######################################################################
 	fim:
 	li		$v0, 10
